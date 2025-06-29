c41511a6b72db056ab84189485366a75b5d14ee9
EcSolticia <ecsolticia@gmail.com>
2025-06-30 02:35:11 +0600

    implement `match_first_header` with error-handling

--- src/absttext.rs
+++ src/absttext.rs
@@ -10,18 +10,23 @@ pub mod matcher {
                 join(" ");
         }
 
-        pub fn condense_block(input_block: &str) -> String {
+        pub fn get_condensed_lines(input_block: &str) -> Vec<String> {
             return input_block.
                 lines().
                 map(condense_block_no_newline).
-                collect::<Vec<String>>().
-                join("\n");
+                collect::<Vec<String>>();
+        }
+
+        pub fn condense_block(input_block: &str) -> String {
+            return get_condensed_lines(input_block).join("\n");
         }
 
     }
 
     pub mod matchers {
         use crate::absttext::matcher::helpers;
+
+        use super::helpers::{condense_block, get_condensed_lines};
 
         // doesn't return the period that follows
         pub fn match_first_sentence(input_block: &str) -> Option<String> {
@@ -38,7 +43,7 @@ pub mod matcher {
         pub fn match_sentence(input_block: &str) -> Option<String> {
             let input_block_without_dot = helpers::condense_block_no_newline(input_block.trim_matches('.'));
             let first_sentence_without_dot = match_first_sentence(input_block)?;
-            
+
             if input_block_without_dot != first_sentence_without_dot {
                 return None;
             } else {
@@ -46,5 +51,18 @@ pub mod matcher {
             }
         }
 
+        pub fn match_first_header(input_block: &str) -> Option<String> {
+            let condensed_lines: String = condense_block(input_block);
+
+            let splits= condensed_lines.split("\n\n");
+            for split in splits {
+                if match_sentence(split) != None {
+                    return match_sentence(split);
+                }
+            }
+
+            return None;
+        }
+        
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod absttext;
 
 fn main() {
-    let evald = absttext::matcher::matchers::match_sentence("    ads   okay ");
+    let evald = absttext::matcher::matchers::match_first_header("Okay, so, I am a header.\n\n what? \n");
     if evald == None {
         println!("Something went wrong!");
     } else {
