7a342c5be9937866c92c160af3570480bdea7c24
EcSolticia <ecsolticia@gmail.com>
2025-06-29 15:40:20 +0600

    remove previous matcherfns and implement excessive whitespace removal

--- src/absttext.rs
+++ src/absttext.rs
@@ -15,39 +15,25 @@ enum BlockType {
 }
 
 pub mod matcher {
-    type MatcherFn = fn(&'static str) -> Option<String>;
+    type MatcherFn = fn(&str) -> Option<String>;
     
     pub mod matchers {
-        use crate::absttext::Paragraph;
 
-        pub fn match_first_sentence(input_block: &'static str) -> Option<String> {
-            let splits: Vec<&str> = input_block.split('.').collect();
-            if splits[0].is_empty() {return None}
-            else {return Some( format!("{}.", splits[0].to_string()) )}
+        fn condense_block_no_newline(input_block: &str) -> String {
+            return input_block.
+                    split_whitespace().
+                    collect::<Vec<&str>>().
+                    join(" ");
         }
 
-        pub fn match_paragraph(input_block: &'static str) -> Option<String> {
-            let mut sentence: String = match_first_sentence(input_block).unwrap();
-            let mut vrest: Vec<&str>;
-            let mut rest: &str = " ";
-            
-            let mut paragraph: String = "".to_string();
-
-            while !(rest.is_empty()) {
-                paragraph.push_str(&sentence);
-
-                vrest = input_block.split(sentence.as_str()).collect();
-
-                if vrest.len() == 2 {
-                    rest = vrest[1];
-                } else {
-                    break;
-                }
-
-                sentence = match_first_sentence(rest).unwrap();
-            }
-
-            return Some(paragraph);
+        pub fn condense_block(input_block: &str) -> Option<String> {
+            return Some(
+                input_block.
+                    lines().
+                    map(condense_block_no_newline).
+                    collect::<Vec<String>>().
+                    join("\n")
+                );
         }
 
     }
