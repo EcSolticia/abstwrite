77364d09b2ebe02d900babbd59baf129a4abf21b
EcSolticia <ecsolticia@gmail.com>
2025-07-01 12:18:02 +0600

    add unit tests

--- src/absttext.rs
+++ src/absttext.rs
@@ -79,3 +79,160 @@ pub mod matcher {
         
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::absttext::matcher::matchers::{match_first_header, match_first_sentence, match_paragraph, match_sentence};
+    
+    #[test]
+    fn test_match_first_sentence_qin_nonperiod_separator() {
+        assert_eq!(
+            match_first_sentence("I am the first sentence! Am I the second sentence?"),
+            Some("I am the first sentence!".to_string())
+        )
+    }
+    #[test]
+    fn test_match_first_sentence_qin_2sentences() {
+        assert_eq!(
+            match_first_sentence("I am the first sentence. I am the second sentence."),
+            Some("I am the first sentence".to_string())
+        )
+    }
+    #[test]
+    fn test_match_first_sentence_qin_period_sentence() {
+        assert_eq!(
+            match_first_sentence("I am a simple sentence with a period."),
+            Some("I am a simple sentence with a period".to_string())
+        )
+    }
+    #[test]
+    fn test_match_first_sentence_qin_noperiod_sentence() {
+        assert_eq!(
+            match_first_sentence("I am a simple sentence without a period"),
+            Some("I am a simple sentence without a period".to_string())
+        )
+    }
+    #[test]
+    fn test_match_first_sentence_qin_no_sentence() {
+        assert_eq!(
+            match_first_sentence(".Where's the sentence this time?"),
+            None
+        )
+    }
+    #[test]
+    fn test_match_first_sentence_qin_periodonly() {
+        assert_eq!(
+            match_first_sentence("."),
+            None
+        )
+    }
+    #[test]
+    fn test_match_first_sentence_qin_empty() {
+        assert_eq!(
+            match_first_sentence(""),
+            None
+        )
+    }
+
+    #[test]
+    fn test_match_sentence_qin_nonperiod_separator() {
+        assert_eq!(
+            match_sentence("I am an individual sentence, or am I? I am another sentence."),
+            Some("I am an individual sentence, or am I?".to_string())
+        )
+    }
+    #[test]
+    fn test_match_sentence_qin_period_sentence() {
+        assert_eq!(
+            match_sentence("I am a simple sentence."),
+            Some("I am a simple sentence".to_string())
+        )
+    }
+    #[test]
+    fn test_match_sentence_qin_noperiod_sentence() {
+        assert_eq!(
+            match_sentence("I am a simple sentence"),
+            Some("I am a simple sentence".to_string())
+        )
+    }
+    #[test]
+    fn test_match_sentence_qin_not_sentence() {
+        assert_eq!(
+            match_sentence("I am a simple sentence. I am another sentence."),
+            None
+        )
+    }
+    #[test]
+    fn test_match_sentence_qin_no_sentence() {
+        assert_eq!(
+            match_sentence(". Who is sentence?"),
+            None
+        )
+    }
+    #[test]
+    fn test_match_sentence_qin_periodonly() {
+        assert_eq!(
+            match_sentence("."),
+            None
+        )
+    }
+    #[test]
+    fn test_match_sentence_qin_empty() {
+        assert_eq!(
+            match_sentence(""),
+            None
+        )
+    }
+
+    #[test]
+    fn test_match_first_header_qin_sentence() {
+        assert_eq!(
+            match_first_header("I am a header."),
+            Some("I am a header".to_string())
+        )
+    }
+    #[test]
+    fn test_match_first_header_qin_multilines() {
+        assert_eq!(
+            match_first_header("Hello there. What is your favorite kind of forest? \n\n Haha, maybe that's a weird question. \n\n What else would you like to see?"),
+            Some("Haha, maybe that's a weird question".to_string())
+        )
+    }
+    #[test]
+    fn test_match_first_header_qin_multilines_nonperiod_separator() {
+        assert_eq!(
+            match_first_header("Hello there! What is your favorite kind of forest? \n\n Haha, maybe that's a weird question. \n\n What else would you like to see?"),
+            Some("Haha, maybe that's a weird question".to_string())
+        )
+    }
+
+    #[test]
+    fn test_match_paragraph_qin_period_separator() {
+        assert_eq!(
+            match_paragraph("Hello there. I am a paragraph."),
+            Some("Hello there. I am a paragraph.".to_string())
+        )
+    }
+    #[test]
+    fn test_match_paragraph_qin_empty() {
+        assert_eq!(
+            match_paragraph(""),
+            None
+        )
+    }
+    #[test]
+    fn test_match_paragraph_qin_periodonly() {
+        assert_eq!(
+            match_paragraph("."),
+            Some(".".to_string())
+        )
+    }
+    #[test]
+    fn test_match_paragraph_qin_sentence() {
+        assert_eq!(
+            match_paragraph("I am a sentence."),
+            Some("I am a sentence.".to_string())
+        )
+    }
+
+}
